cfba52719e3aa367cb8e65d22c0ff0b95b6fa07c
Pedro Sampaio <pedroigorjs@gmail.com>
2021-04-28 17:11:44 -0300

    create Hero seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+Hero.delete_all
+
+10.times do
+  Hero.create name: Faker::Superhero.name
+end
