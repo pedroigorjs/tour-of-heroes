768762338396bf7f6bfbebde5198db043869d26a
Pedro Sampaio <pedroigorjs@gmail.com>
2021-04-30 13:14:52 -0300

    create api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :heroes
+  namespace :api, defaults: { format: :json } do
+    resources :heroes
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
